1cf420a3f082b8d0e502c19edbf6f9a7e609b5e3
Mateus314159 <mateusmiguelsilva641@gmail.com>
2025-06-10 00:28:12 -0300

    corrigindo err odo server

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
 const path = require('path');
-const express = require('express');
+
 
 
 const app = express();
